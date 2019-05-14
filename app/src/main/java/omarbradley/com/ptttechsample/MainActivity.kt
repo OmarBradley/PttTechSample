991945099c9f98f7666514844c2b985f0ffd066e
이재화 <cenacolo34@gmail.com>
2019-05-14 19:53:05 +0900

    ptt 기술서치 중

--- app/src/main/java/omarbradley/com/ptttechsample/MainActivity.kt
+++ app/src/main/java/omarbradley/com/ptttechsample/MainActivity.kt
@@ -1,12 +1,31 @@
 package omarbradley.com.ptttechsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            startActivity(Intent(this, AlarmControlActivity::class.java))
+        }
+
+        button2.setOnClickListener {
+            startActivity(Intent(this, VolumeControlActivity::class.java))
+        }
+
+        button3.setOnClickListener {
+            startActivity(Intent(this, BluetoothActivity::class.java))
+        }
+
     }
+
 }
